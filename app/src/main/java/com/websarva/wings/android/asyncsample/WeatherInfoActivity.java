308e5f4832ec0577b69f0369de503e12723a62ce
kishimotsu72 <kishimotsu72@gmail.com>
2018-06-27 10:42:26 +0900

    P265 2>WeatherInfoReceverクラスを実行する処理を記述する

--- app/src/main/java/com/websarva/wings/android/asyncsample/WeatherInfoActivity.java
+++ app/src/main/java/com/websarva/wings/android/asyncsample/WeatherInfoActivity.java
@@ -1,17 +1,29 @@
 package com.websarva.wings.android.asyncsample;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.view.View;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.List;
+import java.util.Map;
 
 public class WeatherInfoActivity extends AppCompatActivity {
 
@@ -32,7 +44,7 @@ public class WeatherInfoActivity extends AppCompatActivity {
         cityList.add(city);
         city = new HashMap<>();
 
-        city.put("name","神戸");
+        city.put("name", "神戸");
         city.put("id", "280010");
         cityList.add(city);
 
@@ -47,16 +59,52 @@ public class WeatherInfoActivity extends AppCompatActivity {
 
     }
 
-    private class ListItemClickListener implements AdapterView.OnItemClickListener{
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
 
         @Override
-        public void onItemClick(AdapterView<?>parent,View view,int position, long id){
-    Map<String,String> item = (Map<String,String>)parent.getItemAtPosition(position);
-    String cityName = item.get("name");
-String cityId = item.get("id");
-            TextView tvCityName =findViewById(R.id.tvCityName);
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Map<String, String> item = (Map<String, String>) parent.getItemAtPosition(position);
+            String cityName = item.get("name");
+            String cityId = item.get("id");
+            TextView tvCityName = findViewById(R.id.tvCityName);
             tvCityName.setText(cityName + "の天気：");
-}
 
+            TextView tvWeatherTelop = findViewById(R.id.tvWeatherTelop);
+            TextView tvWeatherDesc = findViewById(R.id.tvWeatherDesc);
+            WeatherInfoReceiver receiver = new WeatherInfoReceiver(tvWeatherTelop, tvWeatherDesc);
+            receiver.execute(cityId);
+        }
+
+    }
+
+    private class WeatherInfoReceiver extends AsyncTask<String, String, String> {
+        private TextView _tvWeatherTelop;
+        private TextView _tvWeatherDesc;
+
+        public WeatherInfoReceiver(TextView tvWeatherTelop, TextView tvWeatherDesc) {
+            _tvWeatherTelop = tvWeatherTelop;
+            _tvWeatherDesc = tvWeatherDesc;
+        }
+
+        @Override
+        public String doInBackground(String... params) {
+            String id = params[0];
+            String urlStr = "http://weather.livedoor.com/forecast/webservice/json/v1?city=" + id;
+            String result = "";
+
+            return result;
+
+        }
+
+        @Override
+        public void onPostExecute(String result) {
+
+            String telop = "";
+            String desc = "";
+
+
+            _tvWeatherTelop.setText(telop);
+            _tvWeatherDesc.setText(desc);
+        }
     }
 }
